9b805195168c6ada0753ac9568ea02bbb7fd82d8
Yohey Funada <dev@fulufulu.com>
2018-12-27 21:06:47 +0900

    1秒間隔で実行

--- motion.py
+++ motion.py
@@ -6,7 +6,6 @@
 import RPi.GPIO as GPIO             #importing the RPi.GPIO module
 import time                        #importing the time module
 GPIO.cleanup()                     #to clean up at the end of your script
-led_pin = 37                       #select the pin for the LED
 motion_pin = 35                    #select the pin for the motion sensor
 def init():
   GPIO.setmode(GPIO.BOARD)         #to specify which pin numbering system
@@ -18,10 +17,10 @@ def main():
   while True:
     value=GPIO.input(motion_pin)
     if value!=0:                             #to read the value of a GPIO pin
-      time.sleep(2)        #delay 2ms
+      time.sleep(1)        #delay 2ms
       print "on"                           #print information
     else:
-      time.sleep(2)       #delay 2ms
+      time.sleep(1)       #delay 2ms
       print "off"                         #print information
 
 init()
